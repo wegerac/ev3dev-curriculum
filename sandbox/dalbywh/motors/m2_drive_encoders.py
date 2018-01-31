a754afc029e38417ca7ad4a56f5297f1fadf5ef1
dalbywh <dalbywh@rose-hulman.edu>
2018-01-31 16:31:37 -0500

    Finished m2

--- sandbox/dalbywh/motors/m2_drive_encoders.py
+++ sandbox/dalbywh/motors/m2_drive_encoders.py
@@ -1,19 +1,23 @@
-#!/usr/bin/env python3
+ #!/usr/bin/env python3
 """
 This module lets you practice using the encoder to determine distances while blocking code execution until complete.
 
 You will now use a run_to_rel_pos command to implement the action drive inches action.
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
-
-# TODO: 2. Copy the contents of your m1_drive_timed.py and paste that text into this file below these comments.
+Authors: David Fisher and William Dalby, Michael Kelly, Andrew Weger.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
+import ev3dev.ev3 as ev3
+import time
+# DONE: 2. Copy the contents of your m1_drive_timed.py and paste that text into
+ #  this file below these comments.
 #   If your program says and prints anything at the start change it to print and say "Drive using encoders"
 
-# TODO: 3. Add a beep after the drive motors stop (see code below).  Test your code to hear the beep AFTER movement.
+# DONE: 3. Add a beep after the drive motors stop (see code below).  Test your
+ # code to hear the beep AFTER movement.
 #   ev3.Sound.beep().wait()
 
-# TODO: 4. Instead of using the run_forever, time.sleep, stop pattern switch to using the run_to_rel_pos command.
+# DONE: 4. Instead of using the run_forever, time.sleep, stop pattern switch to
+ #  using the run_to_rel_pos command.
 #   You will need to determine the position_sp value to pass into the run_to_rel_pos command as a named argument.
 #   Assume the diameter of the wheel is 1.3" (close enough).  A 1.3" diameter wheel results in approximately a 4"
 #     circumference, so 360 degrees = 4 inches of travel.
@@ -43,5 +47,39 @@ Authors: David Fisher and PUT_YOUR_NAME_HERE.
 # TODO: 7. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
 #
 # Observations you should make, run_to_rel_pos is easier to use since it uses encoders that are independent of speed.
+
+print("--------------------------------------------")
+print("Drive using encoders")
+print("--------------------------------------------")
+ev3.Sound.speak("Drive using encoders").wait()
+
+# Connect two large motors on output ports B and C
+left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+
+# Check that the motors are actually connected
+assert left_motor.connected
+assert right_motor.connected
+
+
+
+time_s = 1  # Any value other than 0.
+while time_s != 0:
+    inches_target = int (input('How far would you like to go in inches? '))
+    speed = int (input('How fast would you like to go? '))
+    degrees_per_inch = 90
+    motor_turns_needed_in_degrees = inches_target * degrees_per_inch
+
+    print('Here it Is', motor_turns_needed_in_degrees,' ', speed)
+
+    left_motor.run_to_rel_pos(position_sp = motor_turns_needed_in_degrees,speed_sp =
+    speed, stop_action ='brake')
+    right_motor.run_to_rel_pos(position_sp = motor_turns_needed_in_degrees,speed_sp =
+    speed, stop_action ='brake')
+    right_motor.wait_while(ev3.Motor.STATE_RUNNING)
+    ev3.Sound.beep()
+
+print("Goodbye!")
+ev3.Sound.speak("Goodbye").wait()
 
 
