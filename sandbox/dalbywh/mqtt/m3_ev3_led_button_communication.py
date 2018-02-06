aa3dde39e39c7f644a242f34db25cfd0059cb6ba
dalbywh <dalbywh@rose-hulman.edu>
2018-02-05 19:53:53 -0500

    working on mqtt m3

--- sandbox/dalbywh/mqtt/m3_ev3_led_button_communication.py
+++ sandbox/dalbywh/mqtt/m3_ev3_led_button_communication.py
@@ -131,7 +131,7 @@ def handle_button_press(button_state, mqtt_client, button_name):
         #   -- Pass the parameters [button_name] as a list.
         # This is meant to help you learn the mqtt_client.send_message syntax.
         # You can review the code above to understand how button_name is passed into this function.
-        mqtt_client.send_message(button_pressed, [button_name])
+        mqtt_client.send_message('button_name', [button_name])
 
 # TODO: 5. Run this program on your EV3 and run m3_pc_led_button_communication.py on your PC at the same time.
 # This will be the first time you've run a program on the robot today, but you'll remember how to do it (right?).
