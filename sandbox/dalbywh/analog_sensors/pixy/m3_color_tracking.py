2bd34dae432d142b6c3de01eea1a94743e2266f3
Michael Kelly <kellymb@rose-hulman.edu>
2018-02-08 17:05:42 -0500

    PIXY FOLLOW THE BLUE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- sandbox/dalbywh/analog_sensors/pixy/m3_color_tracking.py
+++ sandbox/dalbywh/analog_sensors/pixy/m3_color_tracking.py
@@ -5,8 +5,8 @@ the robot so that it is always facing the color signature.  You will need to tea
 implement the code, then make the robot always face the color as you move it around.  The robot will only spin and never
 move forwards or backwards.
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and Michael Kelly, William Dalby, and Andrew Weger.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import ev3dev.ev3 as ev3
 import time
@@ -24,24 +24,36 @@ def main():
     # This code assumes you have setup the pixy object on the Snatch3r class.
     # Add the pixy property to that class if you have not done so already.
     robot = robo.Snatch3r()
-    robot.pixy.mode = "SIG1"
+    robot.pixy.mode = "SIG2"
     turn_speed = 100
 
     while not robot.touch_sensor.is_pressed:
 
-        # TODO: 2. Read the Pixy values for x and y
+        # DONE: 2. Read the Pixy values for x and y
         # Print the values for x and y
+        x = robot.pixy.value(1)
+        y = robot.pixy.value(2)
+        print("X:", x, "Y:", y)
 
-        # TODO: 3. Use the x value to turn the robot
+        # DONE: 3. Use the x value to turn the robot
         #   If the Pixy x value is less than 150 turn left (-turn_speed, turn_speed)
         #   If the Pixy x value is greater than 170 turn right (turn_speed, -turn_speed)
         #   If the Pixy x value is between 150 and 170 stop the robot
         # Continuously track the color until the touch sensor is pressed to end the program.
+        if x < 150:
+            robot.right_motor.run_forever(speed_sp=turn_speed)
+            robot.left_motor.run_forever(speed_sp=-turn_speed)
+        elif x > 170:
+            robot.right_motor.run_forever(speed_sp=-turn_speed)
+            robot.left_motor.run_forever(speed_sp=turn_speed)
+        else:
+            robot.stop()
 
 
 
         time.sleep(0.25)
 
+    robot.stop()
     print("Goodbye!")
     ev3.Sound.speak("Goodbye").wait()
 
