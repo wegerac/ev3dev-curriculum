ada3173b121ed6063f123660059726b42cca0e66
dalbywh <dalbywh@rose-hulman.edu>
2018-02-12 16:10:12 -0500

    Fixed some PEP8 errors and started the individual project by starting a GUI

---
+++ projects/dalbywh/dalbywh_PC.py
@@ -0,0 +1,198 @@
+"""
+The purpose of this project is to create an assisted driving car
+It will be able to be controlled with both a GUI and a controller
+
+The GUI will have tabs to switch out the different modes
+
+The GUI will also have an options tab to eliminate certain options
+
+The Speed will be controlled in two ways
+    GUI - slider
+    Controller - Specific Channel will change motor speeds
+
+The car will drive
+    If it sees a line, it will correct back onto the road while continuing
+    forward
+
+    If it sees a wall it will beep
+        If the wall gets too close, the robot will stop and say something
+
+The arm can also be adjusted
+    It will only move while the button is pressed rather than all the way up or
+    down
+
+    the speed can be adjusted in the same way the other motors can
+
+    If arm motor goes past certain spot, IR no longer runs
+
+    Will warn user that this is the case
+
+While the robot runs
+    The GUI will draw out the path that the robot moves.
+        Probably going to be a simple_turtle
+
+
+
+
+
+Authors: William Dalby
+"""
+
+import tkinter
+from tkinter import ttk
+import time
+import math
+import robot_controller as robo
+
+def main():
+    root = tkinter.Tk()
+    root.title = 'Robot Controller'
+    controller_frame = ttk.Frame(root, padding = 5)
+    controller_frame.grid()
+
+    button_example = ttk.Button(controller_frame, text = 'Blah')
+    button_example.grid()
+
+    root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main()
