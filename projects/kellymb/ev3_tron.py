0835dc6fc49e9c05d813c63d3f3e8db1ed289728
Michael Kelly <kellymb@rose-hulman.edu>
2018-02-14 15:52:38 -0500

    tron class

--- projects/kellymb/ev3_tron.py
+++ projects/kellymb/ev3_tron.py
@@ -2,9 +2,12 @@
  including light cycle and Battle tanks """
 
 import mqtt_remote_method_calls as com
-import Tron_controller
+import Tron_controller as tron
 
 def main():
+    robot = tron.Tron()
+    mqtt_client = com.MqttClient(robot)
+    mqtt_client.connect_to_pc()
 
 
 
